c6db4da0d7f6cabd7e56909ca8409fa1693a229c
Joshi <Sachin.Joshi@coxautoinc.com>
2017-07-22 16:08:59 -0400

    Initial commit

---
+++ core/updateStack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+aws cloudformation update-stack --stack-name $1 --template-url https://s3-us-west-2.amazonaws.com/dms-cf-templates/cfn_master.json --parameters https://s3-us-west-2.amazonaws.com/dms-cf-templates/launch_params.json --profile=personal
+
+# aws cloudformation create-stack --stack-name $1 --template-body file://cfn-master.json --parameters file://launch-params.json --capabilities CAPABILITY_IAM --disable-rollback
+    
